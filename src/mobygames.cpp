7d5b607d2b50b041b6744d3338fecc0ff114d618
Marcel Saegebarth <marc@mos6581.de>
2023-05-09 12:58:19 +0200

    Fix Bally Astrocade for Mobygames

--- src/mobygames.cpp
+++ src/mobygames.cpp
@@ -413,7 +413,7 @@ QString MobyGames::getPlatformId(const QString platform)
     return "na";
   } else if(platform == "arcadia") {
     return "162";
-  } else if(platform == "astrocde") {
+  } else if(platform == "astrocade") {
     return "160";
   } else if(platform == "atari800") {
     return "39";
